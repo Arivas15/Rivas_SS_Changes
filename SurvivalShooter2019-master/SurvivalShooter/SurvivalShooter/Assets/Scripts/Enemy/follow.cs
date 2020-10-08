081c5b3591a356da2c5383c704f597e8b41304fe
Arivas15 <54857672+Arivas15@users.noreply.github.com>
2020-10-08 12:25:16 -0500

    Final Changes
    1. enemy health
    2. camera shake
    3. heartbeat
    4. dust trail
    5. zoom in on player death

---
+++ SurvivalShooter2019-master/SurvivalShooter/SurvivalShooter/Assets/Scripts/Enemy/follow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    [SerializeField]
+    Transform followTarget;
+
+    [SerializeField]
+    bool x = false;
+
+    [SerializeField]
+    bool y = false;
+
+    [SerializeField]
+    bool z = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = followTarget.position;
+
+        if(!x)
+        {
+            newPos.x = transform.position.x;
+        }
+
+        if(!y)
+        {
+            newPos.y = transform.position.y;
+        }
+
+        if(!z)
+        {
+            newPos.z = transform.position.z;
+        }
+        transform.position = newPos;
+    }
+}
